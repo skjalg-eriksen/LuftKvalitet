24ee40a4ca5a120c5dd92b288a5fbfd1e41d7ae7
s300386 <s300386@stud.hioa.no>
2018-03-06 08:55:10 +0100

    app works locally nad will update once an hour and will diplsay the result with the /show

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 from cloudant import Cloudant
-from flask import Flask, render_template, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify, send_file, url_for
+import os
 from urllib2 import Request, urlopen, URLError
 import atexit
 import cf_deployment_tracker
 import os
 import json
 import math
+
+
+import atexit
+from apscheduler.scheduler import Scheduler
 
 import numpy as np
 from pandas.io.json import json_normalize
@@ -18,7 +23,7 @@ import utm
 
 import dataset
 from krige import SVh, SV, C, spherical, opt, cvmodel, krige
-from mpl_toolkits.basemap import Basemap
+#from mpl_toolkits.basemap import Basemap
 import datetime
 
 #map corners
@@ -27,6 +32,77 @@ BOTTOM = 6638524.509011956;
 RIGHT = 605365.439142052; 
 TOP = 6648891.652304975;
 
+cron = Scheduler(daemon=True)
+cron.start()
+
+@cron.interval_schedule(hours=1)
+def job_function():
+  print("AAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAa\nIM WORKING\naaaaaaaaaaaaaaaah!")
+  z = dataset.data();
+  # part of our data set recording porosity
+  P = np.array( z[['x','y','value']] )
+  # bandwidth, plus or minus 250 meters
+  bw = 15500
+  # lags in 500 meter increments from zero to 10,000
+  # hs = np.arange(0,10500,bw)
+  hs = np.arange(0,20000, bw)
+  sv = SV( P, hs, bw )
+
+
+  X0, X1 = 0, RIGHT-LEFT
+  Y0, Y1 = 0, TOP-BOTTOM
+
+
+  nx = 48
+  ny = 60
+  num_points = 7
+  Z = np.zeros((ny,nx))
+  dx, dy = (X1-X0)/float(nx), (Y1-Y0)/float(ny)
+  for i in range(nx):
+      print (i),
+      for j in range(ny):
+          x = X0 + i*dx
+          y = Y0 + j*dy
+          Z[j,i] = krige( P, spherical, hs, bw, (x, y), num_points )
+
+  cdict = {'red':   ((0.0, 1.0, 1.0),
+                   (0.5, 225/255., 225/255. ),
+                   (0.75, 0.141, 0.141 ),
+                   (1.0, 0.0, 0.0)),
+         'green': ((0.0, 1.0, 1.0),
+                   (0.5, 57/255., 57/255. ),
+                   (0.75, 0.0, 0.0 ),
+                   (1.0, 0.0, 0.0)),
+         'blue':  ((0.0, 0.376, 0.376),
+                   (0.5, 198/255., 198/255. ),
+                   (0.75, 1.0, 1.0 ),
+                   (1.0, 0.0, 0.0)) }
+
+  my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap', cdict, 256)
+
+  fig, ax = subplots()
+  fig.dpi=400
+  H = np.zeros_like( Z )
+  for i in range( Z.shape[0] ):
+      for j in range( Z.shape[1] ):
+          H[i,j] = np.round( Z[i,j]*3 )
+          
+  #m = Basemap(llcrnrlon=10.662291,llcrnrlat= 59.873800,urcrnrlon=10.887139,urcrnrlat=59.963886,
+  #             resolution='f',projection='tmerc',lon_0=6.7806151842031,lat_0=60.479443366542, ax = ax)
+  #m.drawcoastlines()
+
+  #ax.matshow( H, cmap=my_cmap, interpolation='nearest' )
+  ax.imshow(H, cmap=my_cmap, origin='lower', interpolation='nearest', alpha=0.7, extent=[X0, X1, Y0, Y1])
+  sc = ax.scatter( z.x, z.y, cmap=my_cmap, c=z.value, linewidths=0.75, s=50 )
+  #xlim(0,nx) ; ylim(0,ny)
+  plt.colorbar(sc)
+  name='component: ' + str(z['component'].iloc[0]) + ', date: ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+  #fig.suptitle(name, fontsize=14)
+  savefig('static/res/'+str(name), fmt='png', dpi=200 )
+
+
+# Shutdown your cron thread if the web process is stopped
+atexit.register(lambda: cron.shutdown(wait=False))
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -40,7 +116,13 @@ port = int(os.getenv('PORT', 8000))
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
+@app.route('/show')
+def show():
+  names = os.listdir( os.path.join( app.static_folder, 'res'))
+  #files = url_for('static', filename=os.path.join('res', choice(names)))
+  return render_template('show.html', files=names)
+
 @app.route('/data')
 def data():
   # fetches data from nilu
@@ -145,8 +227,8 @@ def kriging_plot():
   Y0, Y1 = 0, TOP-BOTTOM
 
 
-  nx = 50
-  ny = 47
+  nx = 48
+  ny = 60
   num_points = 7
   Z = np.zeros((ny,nx))
   dx, dy = (X1-X0)/float(nx), (Y1-Y0)/float(ny)
@@ -189,9 +271,9 @@ def kriging_plot():
   #xlim(0,nx) ; ylim(0,ny)
   plt.colorbar(sc)
   name='component: ' + str(z['component'].iloc[0]) + ', date: ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-  fig.suptitle(name, fontsize=14)
-  savefig( 'misc/'+str(name), fmt='png', dpi=200 )
-  return send_file('misc/'+str(name) + '.png', mimetype='image/gif')
+  #fig.suptitle(name, fontsize=14)
+  savefig('static/res/'+str(name), fmt='png', dpi=200 )
+  return render_template('show.html', files=names)
   
 
 @app.route('/hello')
