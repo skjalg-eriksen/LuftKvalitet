98c935a0699b3b409d7c06b253fc076cf37e29b6
s300386 <s300386@stud.hioa.no>
2018-03-20 22:19:45 +0100

    played around with database and images

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-from cloudant import Cloudant
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
+from cloudant.document import Document
 from flask import Flask, render_template, request, jsonify, send_file, url_for
 import os
 from urllib2 import Request, urlopen, URLError
@@ -25,6 +28,7 @@ import dataset
 from krige import SVh, SV, C, spherical, opt, cvmodel, krige
 #from mpl_toolkits.basemap import Basemap
 import datetime
+from base64 import b64encode
 
 #map corners
 LEFT = 593065.1648494017; 
@@ -35,75 +39,6 @@ TOP = 6648891.652304975;
 cron = Scheduler(daemon=True)
 cron.start()
 
-@cron.interval_schedule(hours=1)
-def job_function():
-  print("AAAAAAAAAAAAAAAAAAAAAAAAA\nAAAAAAAAAa\nIM WORKING\naaaaaaaaaaaaaaaah!")
-  z = dataset.data();
-  # part of our data set recording porosity
-  P = np.array( z[['x','y','value']] )
-  # bandwidth, plus or minus 250 meters
-  bw = 15500
-  # lags in 500 meter increments from zero to 10,000
-  # hs = np.arange(0,10500,bw)
-  hs = np.arange(0,20000, bw)
-  sv = SV( P, hs, bw )
-
-
-  X0, X1 = 0, RIGHT-LEFT
-  Y0, Y1 = 0, TOP-BOTTOM
-
-
-  nx = 48
-  ny = 60
-  num_points = 7
-  Z = np.zeros((ny,nx))
-  dx, dy = (X1-X0)/float(nx), (Y1-Y0)/float(ny)
-  for i in range(nx):
-      print (i),
-      for j in range(ny):
-          x = X0 + i*dx
-          y = Y0 + j*dy
-          Z[j,i] = krige( P, spherical, hs, bw, (x, y), num_points )
-
-  cdict = {'red':   ((0.0, 1.0, 1.0),
-                   (0.5, 225/255., 225/255. ),
-                   (0.75, 0.141, 0.141 ),
-                   (1.0, 0.0, 0.0)),
-         'green': ((0.0, 1.0, 1.0),
-                   (0.5, 57/255., 57/255. ),
-                   (0.75, 0.0, 0.0 ),
-                   (1.0, 0.0, 0.0)),
-         'blue':  ((0.0, 0.376, 0.376),
-                   (0.5, 198/255., 198/255. ),
-                   (0.75, 1.0, 1.0 ),
-                   (1.0, 0.0, 0.0)) }
-
-  my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap', cdict, 256)
-
-  fig, ax = subplots()
-  fig.dpi=400
-  H = np.zeros_like( Z )
-  for i in range( Z.shape[0] ):
-      for j in range( Z.shape[1] ):
-          H[i,j] = np.round( Z[i,j]*3 )
-          
-  #m = Basemap(llcrnrlon=10.662291,llcrnrlat= 59.873800,urcrnrlon=10.887139,urcrnrlat=59.963886,
-  #             resolution='f',projection='tmerc',lon_0=6.7806151842031,lat_0=60.479443366542, ax = ax)
-  #m.drawcoastlines()
-
-  #ax.matshow( H, cmap=my_cmap, interpolation='nearest' )
-  ax.imshow(H, cmap=my_cmap, origin='lower', interpolation='nearest', alpha=0.7, extent=[X0, X1, Y0, Y1])
-  sc = ax.scatter( z.x, z.y, cmap=my_cmap, c=z.value, linewidths=0.75, s=50 )
-  #xlim(0,nx) ; ylim(0,ny)
-  plt.colorbar(sc)
-  name='component: ' + str(z['component'].iloc[0]) + ', date: ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
-  #fig.suptitle(name, fontsize=14)
-  savefig('/'+str(name), fmt='png', dpi=200 )
-
-
-# Shutdown your cron thread if the web process is stopped
-atexit.register(lambda: cron.shutdown(wait=False))
-
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
 
@@ -113,27 +48,53 @@ app = Flask(__name__)
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
 
+
+db_name = 'luftkvalitet_db'
+client = None
+db = None
+
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.getenv('VCAP_SERVICES'))
+    print('Found VCAP_SERVICES')
+    if 'cloudantNoSQLDB' in vcap:
+        creds = vcap['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+elif os.path.isfile('vcap-local.json'):
+    with open('vcap-local.json') as f:
+        vcap = json.load(f)
+        print('Found local VCAP_SERVICES')
+        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+        user = creds['username']
+        password = creds['password']
+        url = 'https://' + creds['host']
+        client = Cloudant(user, password, url=url, connect=True)
+        db = client.create_database(db_name, throw_on_exists=False)
+        
+
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/show')
-def show():
-  names = os.listdir( './')
-  #files = url_for('static', filename=os.path.join('res', choice(names)))
-  return render_template('show.html', files=names)
-
-@app.route('/data')
-def data():
-  # fetches data from nilu
-  data = urlopen('https://api.nilu.no/aq/utd.json?areas=Oslo').read()
-  # turns it into a json object
-  jdata = json.loads(data)
-  # puts it in a dataframe
-  fdata = json_normalize(jdata)
-  # picks out the wanted values
-  # fdata = DataFrame(fdata, columns=['latitude','longitude', 'value', 'unit', 'component'] )
-  return fdata.to_html()
+    
+@app.route('/db_info')
+def db_info():
+  client = Cloudant(user, password, url=url, connect=True)
+  db = client.create_database(db_name, throw_on_exists=False)
+  return str(db.exists())
+  return ('databases: {0}'.format( client.all_dbs() ));
+  
+@app.route('/db_docs/<name>')
+def db_docs(name):
+  client = Cloudant(user, password, url=url, connect=True)
+  db = client.create_database(db_name, throw_on_exists=False)
+  
+  img = db.all_docs().get_attachment(name, name, default=None).read()
+  img_b64 = base64.b64encode(img)
+  return render_template('testimg.html', img = img_b64)
+  #return jsonify(list(map(lambda doc: doc.get_attachment('5845e1677733c3558233c0ee.png', arrachment), db) ));
 
 @app.route('/data_min')
 def data_min():
@@ -222,10 +183,8 @@ def kriging_plot():
   hs = np.arange(0,20000, bw)
   sv = SV( P, hs, bw )
 
-
   X0, X1 = 0, RIGHT-LEFT
   Y0, Y1 = 0, TOP-BOTTOM
-
 
   nx = 48
   ny = 60
@@ -272,9 +231,74 @@ def kriging_plot():
   plt.colorbar(sc)
   name='component: ' + str(z['component'].iloc[0]) + ', date: ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
   #fig.suptitle(name, fontsize=14)
-  savefig('./'+str(name), fmt='png', dpi=200 )
+  #savefig(app.root_path+'/'+str(name), fmt='png', dpi=200 )
+  
   return render_template('show.html', files=names)
   
+@cron.interval_schedule(hours=3)
+def job_function():
+  print("Running kriging");
+  z = dataset.data();
+  # part of our data set recording porosity
+  P = np.array( z[['x','y','value']] )
+  # bandwidth, plus or minus 250 meters
+  bw = 15500
+  # lags in 500 meter increments from zero to 10,000
+  # hs = np.arange(0,10500,bw)
+  hs = np.arange(0,20000, bw)
+  sv = SV( P, hs, bw )
+
+
+  X0, X1 = 0, RIGHT-LEFT
+  Y0, Y1 = 0, TOP-BOTTOM
+
+
+  nx = 48
+  ny = 60
+  num_points = 7
+  Z = np.zeros((ny,nx))
+  dx, dy = (X1-X0)/float(nx), (Y1-Y0)/float(ny)
+  for i in range(nx):
+      print (i),
+      for j in range(ny):
+          x = X0 + i*dx
+          y = Y0 + j*dy
+          Z[j,i] = krige( P, spherical, hs, bw, (x, y), num_points )
+
+  cdict = {'red':   ((0.0, 1.0, 1.0),
+                   (0.5, 225/255., 225/255. ),
+                   (0.75, 0.141, 0.141 ),
+                   (1.0, 0.0, 0.0)),
+         'green': ((0.0, 1.0, 1.0),
+                   (0.5, 57/255., 57/255. ),
+                   (0.75, 0.0, 0.0 ),
+                   (1.0, 0.0, 0.0)),
+         'blue':  ((0.0, 0.376, 0.376),
+                   (0.5, 198/255., 198/255. ),
+                   (0.75, 1.0, 1.0 ),
+                   (1.0, 0.0, 0.0)) }
+
+  my_cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap', cdict, 256)
+
+  fig, ax = subplots()
+  fig.dpi=400
+  H = np.zeros_like( Z )
+  for i in range( Z.shape[0] ):
+      for j in range( Z.shape[1] ):
+          H[i,j] = np.round( Z[i,j]*3 )
+
+  #ax.matshow( H, cmap=my_cmap, interpolation='nearest' )
+  ax.imshow(H, cmap=my_cmap, origin='lower', interpolation='nearest', alpha=0.7, extent=[X0, X1, Y0, Y1])
+  sc = ax.scatter( z.x, z.y, cmap=my_cmap, c=z.value, linewidths=0.75, s=50 )
+  #xlim(0,nx) ; ylim(0,ny)
+  plt.colorbar(sc)
+  name='component: ' + str(z['component'].iloc[0]) + ', date: ' + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+  #fig.suptitle(name, fontsize=14)
+  savefig(str(name), fmt='png', dpi=200 )
+  
+# Shutdown your cron thread if the web process is stopped
+atexit.register(lambda: cron.shutdown(wait=False))
+atexit.register(client.disconnect())
 
 @app.route('/hello')
 def hello():
